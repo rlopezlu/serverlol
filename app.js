2340dad80fb7c27b34ece6e27d17b7d9087a1b4f
Raul Lopez Luna <raullopez2094@gmail.com>
2018-02-26 20:41:07 -0800

    setup middleware

--- app.js
+++ app.js
@@ -21,6 +21,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//enable cors 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use('/', index);
 
 // catch 404 and forward to error handler
